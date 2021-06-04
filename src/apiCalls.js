b7cabceb448dfa727428a1691586d5fa8181ae1f
Taylor Galloway <gallowaytaylor@outlook.com>
2021-06-04 17:30:11 -0500

    Add basic postApiData function

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -3,3 +3,21 @@ export const fetchApiData = (type) => {
     .then(response => response.json())
     .catch(err => console.log("API error"))
 };
+
+export const postApiData = (type, data) => {
+  return fetch(`http://localhost:3001/api/v1/${type}`, {
+    method: 'POST',
+    body: JSON.stringify({
+      "userID": integer,
+      "date": string,
+      "hoursSlept": integer,
+      "sleepQuality": integer}
+    }),
+    headers: {
+      'Content-type': 'application/json'
+    }
+  })
+  .then(response => response.json())
+  .then(data => console.log("Hooray", data))
+  .catch(err => console.log("API error"))
+}
