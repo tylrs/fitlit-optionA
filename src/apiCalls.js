bc4b3ea76f6c5ea434f534a138a1762fb71ab48b
Taylor Galloway <gallowaytaylor@outlook.com>
2021-06-04 18:56:01 -0500

    Add post API call

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -8,16 +8,16 @@ export const postApiData = (type, data) => {
   return fetch(`http://localhost:3001/api/v1/${type}`, {
     method: 'POST',
     body: JSON.stringify({
-      "userID": integer,
-      "date": string,
-      "hoursSlept": integer,
-      "sleepQuality": integer}
+      "userID": data.userId,
+      "date": data.todayDate,
+      "hoursSlept": data.hoursSlept,
+      "sleepQuality": data.sleepQuality
     }),
     headers: {
       'Content-type': 'application/json'
     }
   })
-  .then(response => response.json())
+  .then((response) => response.json())
   .then(data => console.log("Hooray", data))
   .catch(err => console.log("API error"))
 }
