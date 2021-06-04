9ba20ae4b56936ebaf66d0008adfe8e79c7a8341
Alex Kio <alexandermartelkio@gmail.com>
2021-06-04 17:35:03 -0400

    Organize User tests

--- test/User-test.js
+++ test/User-test.js
@@ -88,22 +88,6 @@ describe('User', function() {
     ]
     expect(user.addDailyOunces("2019/06/15")).to.equal(2);
   });
-  describe('updateSleep', function() {
-    beforeEach(() => {
-      user.updateSleep("2019/06/15", 7, 4.7);
-      user.updateSleep("2019/07/14", 6, 4);
-      user.updateSleep("2019/08/04", 8, 5.4);
-    })
-    it('should update user\'s quality of sleep record', function() {
-      expect(user.sleepQualityRecord.length).to.equal(3);
-    });
-    it('should update user\'s average hours of sleep', function() {
-      expect(user.hoursSleptAverage).to.equal('7.0');
-    });
-    it('should update user\'s average quality of sleep', function() {
-      expect(user.sleepQualityAverage).to.equal('4.7');
-    });
-  })
   it('calculateAverageHoursThisWeek should calculate average sleep hours for week before given date', function() {
     user.sleepHoursRecord = [{date: "2019/09/22", hours: 9.6}, {date: "2019/09/21", hours: 8.2}, {date: "2019/09/20", hours: 9.9}, {date: "2019/09/19", hours: 4.2}, {date: "2019/09/18", hours: 9.5}, {date: "2019/09/17", hours: 7.8}, {date: "2019/09/16", hours: 10.2}, {date: "2019/09/15", hours: 5.7}, {date: "2019/09/14", hours: 8.8}, {date: "2019/09/13", hours: 4.6}, {date: "2019/09/12", hours: 5.3}];
     expect(user.calculateAverageHoursThisWeek('2019/09/21')).to.equal('7.9');
@@ -274,4 +258,20 @@ describe('User', function() {
     user.findFriendsTotalStepsForWeek(users, '2019/06/29');
     expect(user.friendsActivityRecords).to.deep.equal([{"id": 4, "totalWeeklySteps": 734}, {"id": 16, "totalWeeklySteps": 248}, {"id": 8, "totalWeeklySteps": 34}]);
   });
+  describe('updateSleep', function() {
+    beforeEach(() => {
+      user.updateSleep("2019/06/15", 7, 4.7);
+      user.updateSleep("2019/07/14", 6, 4);
+      user.updateSleep("2019/08/04", 8, 5.4);
+    })
+    it('should update user\'s quality of sleep record', function() {
+      expect(user.sleepQualityRecord.length).to.equal(3);
+    });
+    it('should update user\'s average hours of sleep', function() {
+      expect(user.hoursSleptAverage).to.equal('7.0');
+    });
+    it('should update user\'s average quality of sleep', function() {
+      expect(user.sleepQualityAverage).to.equal('4.7');
+    });
+  })
 });
