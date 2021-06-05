268278acd47ad9912a9717cbd4996abc995d9702
Alex Kio <alexandermartelkio@gmail.com>
2021-06-04 22:24:57 -0400

    Refactor beforeEach and some tests

--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -1,116 +1,127 @@
 import { expect } from 'chai';
-
 import UserRepository from '../src/UserRepository';
 import User from '../src/User';
 import Sleep from '../src/Sleep';
+import Activity from '../src/Activity';
+import Hydration from '../src/Hydration';
+import Necessity from '../src/Necessity';
+import { userTestData, sleepTestData, activityTestData, hydrationTestData } from './sampleData';
 
-describe('UserRepository', function() {
-  let user1, user2, user3, userRepository, sleep1, sleep2, sleep3, sleepData, userData;
+describe('userRepository', function() {
+  let user, usersData, userTestRepository, necessity;
+  // let necessity, user1, user2, user3, userTestRepository, sleep1, sleep2, sleep3, sleepData, userTestData;
   beforeEach(() => {
-    user1 = new User({
-      'id': 1,
-      'name': 'Luisa Hane',
-      'address': '15195 Nakia Tunnel, Erdmanport VA 19901-1697',
-      'email': 'Diana.Hayes1@hotmail.com',
-      'strideLength': 4.3,
-      'dailyStepGoal': 10000,
-      'friends': [
-        16,
-        4,
-        8
-      ]
-    })
-    user2 = new User({
-      "id": 2,
-      "name": "Jarvis Considine",
-      "address": "30086 Kathryn Port, Ciceroland NE 07273",
-      "email": "Dimitri.Bechtelar11@gmail.com",
-      "strideLength": 4.5,
-      "dailyStepGoal": 5000,
-      "friends": [
-        9,
-        18,
-        24,
-        19
-      ]
-    })
-    user3 = new User({
-      "id": 3,
-      "name": "Herminia Witting",
-      "address": "85823 Bosco Fork, East Oscarstad MI 85126-5660",
-      "email": "Elwin.Tromp@yahoo.com",
-      "strideLength": 4.4,
-      "dailyStepGoal": 15000,
-      "friends": [
-        19,
-        11,
-        42,
-        33
-      ]
-    })
+    usersData = userTestData.map(user => new User(user));
+    userTestRepository = new UserRepository(usersData, sleepTestData, activityTestData, hydrationTestData);
+    necessity = new Necessity(userTestRepository);
+    sleepTestData.forEach(sleep => new Sleep(sleep));
+    activityTestData.forEach(activity => new Activity(activity));
+    hydrationTestData.forEach(hydration => new Hydration(hydration));
+    user = userTestRepository.users[0];
+    // user.findFriendsNames(userTestRepository.users);
+  });
+
+    // user1 = new User({
+    //   'id': 1,
+    //   'name': 'Luisa Hane',
+    //   'address': '15195 Nakia Tunnel, Erdmanport VA 19901-1697',
+    //   'email': 'Diana.Hayes1@hotmail.com',
+    //   'strideLength': 4.3,
+    //   'dailyStepGoal': 10000,
+    //   'friends': [
+    //     16,
+    //     4,
+    //     8
+    //   ]
+    // })
+    // user2 = new User({
+    //   "id": 2,
+    //   "name": "Jarvis Considine",
+    //   "address": "30086 Kathryn Port, Ciceroland NE 07273",
+    //   "email": "Dimitri.Bechtelar11@gmail.com",
+    //   "strideLength": 4.5,
+    //   "dailyStepGoal": 5000,
+    //   "friends": [
+    //     9,
+    //     18,
+    //     24,
+    //     19
+    //   ]
+    // })
+    // user3 = new User({
+    //   "id": 3,
+    //   "name": "Herminia Witting",
+    //   "address": "85823 Bosco Fork, East Oscarstad MI 85126-5660",
+    //   "email": "Elwin.Tromp@yahoo.com",
+    //   "strideLength": 4.4,
+    //   "dailyStepGoal": 15000,
+    //   "friends": [
+    //     19,
+    //     11,
+    //     42,
+    //     33
+    //   ]
+    // })
     //OG code below:
-    // userRepository = new UserRepository();
-    //userRepository.users.push(user1, user2, user3);
-    userData = [user1, user2, user3];
-    userRepository = new UserRepository(userData);
+    // userTestRepository = new userTestRepository();
+    //userTestRepository.users.push(user1, user2, user3);
+    // userTestData = [user1, user2, user3];
+    // userTestRepository = new userRepository(userTestData);
 
-    sleep1 = new Sleep({
-      "userID": 1,
-      "date": "2019/06/16",
-      "hoursSlept": 6.1,
-      "sleepQuality": 1000
-    }, userRepository);
-    sleep2 = new Sleep({
-      "userID": 2,
-      "date": "2019/06/15",
-      "hoursSlept": 7.3,
-      "sleepQuality": 500
-    }, userRepository);
-    sleep3 = new Sleep({
-      "userID": 3,
-      "date": "2019/06/15",
-      "hoursSlept": 9.3,
-      "sleepQuality": 1.4
-    }, userRepository);
+    // sleep1 = new Sleep({
+    //   "userID": 1,
+    //   "date": "2019/06/16",
+    //   "hoursSlept": 6.1,
+    //   "sleepQuality": 1000
+    // });
+    // sleep2 = new Sleep({
+    //   "userID": 2,
+    //   "date": "2019/06/15",
+    //   "hoursSlept": 7.3,
+    //   "sleepQuality": 500
+    // });
+    // sleep3 = new Sleep({
+    //   "userID": 3,
+    //   "date": "2019/06/15",
+    //   "hoursSlept": 9.3,
+    //   "sleepQuality": 1.4
+    // });
 
-    sleepData = [{
-      "userID": 1,
-      "date": "2019/06/15",
-      "hoursSlept": 6.1,
-      "sleepQuality": 100
-    }, {
-      "userID": 2,
-      "date": "2019/06/15",
-      "hoursSlept": 7.3,
-      "sleepQuality": 1500
-    }, {
-      "userID": 3,
-      "date": "2019/06/15",
-      "hoursSlept": 9.3,
-      "sleepQuality": 1.4
-    }];
-  })
+    // sleepData = [{
+    //   "userID": 1,
+    //   "date": "2019/06/15",
+    //   "hoursSlept": 6.1,
+    //   "sleepQuality": 100
+    // }, {
+    //   "userID": 2,
+    //   "date": "2019/06/15",
+    //   "hoursSlept": 7.3,
+    //   "sleepQuality": 1500
+    // }, {
+    //   "userID": 3,
+    //   "date": "2019/06/15",
+    //   "hoursSlept": 9.3,
+    //   "sleepQuality": 1.4
+    // }];
+
   it('should be a function', function() {
     expect(UserRepository).to.be.a('function');
   });
   it('should be an instance of user repository', function() {
-    expect(userRepository).to.be.an.instanceof(UserRepository);
+    expect(userTestRepository).to.be.an.instanceof(UserRepository);
   });
   it('should hold an array of users', function() {
-    expect(userRepository.users).to.deep.equal([user1, user2, user3]);
-    expect(userRepository.users.length).to.equal(3);
+    expect(userTestRepository.users).to.deep.equal(usersData);
+    expect(userTestRepository.users.length).to.equal(3);
   });
   it('getUser should return user object when given a user id', function() {
-    expect(userRepository.getUser(2)).to.equal(user2);
+    expect(userTestRepository.getUser(2)).to.equal(usersData[1]);
   })
   it('calculateAverageStepGoal should return average step goal for all users', function() {
-    expect(userRepository.calculateAverageStepGoal()).to.equal(10000);
+    expect(userTestRepository.calculateAverageStepGoal()).to.equal(20000 / 3);
   })
-  it('calculateAverageSleepQuality should return average sleep quality for all users', function() {
-    user1.sleepQualityAverage = 3.3;
-    user2.sleepQualityAverage = 5;
-    user3.sleepQualityAverage = 1;
-    expect(userRepository.calculateAverageSleepQuality()).to.equal(3.1);
+  it.only('calculateAverageSleepQuality should return average sleep quality for all users', function() {
+    expect(userTestRepository.calculateAverageSleepQuality()).to.equal((11.6 / 3).toFixed(2));
   });
   it('should have a method that calculates friends average ounces of water', function() {
     user1.ouncesRecord = [
@@ -128,60 +139,60 @@ describe('UserRepository', function() {
       {"2019/06/15": 1},
       {"2019/06/16": 4}
     ]
-    expect(userRepository.calculateAverageDailyWater("2019/06/16")).to.equal(5)
+    expect(userTestRepository.calculateAverageDailyWater("2019/06/16")).to.equal(5)
   });
   it('should have a method that finds the best sleepers', function() {
-    expect(userRepository.findBestSleepers("2019/06/16")).to.deep.equal([user1, user2]);
+    expect(userTestRepository.findBestSleepers("2019/06/16")).to.deep.equal([user1, user2]);
   });
 
   //Original getLongestSleepers and getWorstSleepers functions
   // it('should have a method that finds the longest sleepers', function() {
-  //   expect(userRepository.getLongestSleepers("2019/06/15")).to.equal(3);
+  //   expect(userTestRepository.getLongestSleepers("2019/06/15")).to.equal(3);
   // });
   //
   // it('should have a method that finds the worst sleepers', function() {
-  //   expect(userRepository.getWorstSleepers("2019/06/15")).to.equal(1);
+  //   expect(userTestRepository.getWorstSleepers("2019/06/15")).to.equal(1);
   // });
 
   it('should have a method that finds the longest sleepers', function() {
-    expect(userRepository.getSleeper("2019/06/15", "best")).to.equal(3);
+    expect(userTestRepository.getSleeper("2019/06/15", "best")).to.equal(3);
   });
 
   it('should have a method that finds the worst sleepers', function() {
-    expect(userRepository.getSleeper("2019/06/15", "worst")).to.equal(1);
+    expect(userTestRepository.getSleeper("2019/06/15", "worst")).to.equal(1);
   });
 
   // it('should have a method that calculates average number of stairs for users', function() {
   //   user1.activityRecord = [{date: "2019/09/17", flightsOfStairs: 10}, {date: "2019/09/17", flightsOfStairs: 15}];
   //   user2.activityRecord = [{date: "2019/09/16", flightsOfStairs: 8}, {date: "2019/09/17", flightsOfStairs: 4}];
-  //   expect(userRepository.calculateAverageStairs("2019/09/17")).to.equal(10);
+  //   expect(userTestRepository.calculateAverageStairs("2019/09/17")).to.equal(10);
   // })
   // it('should have a method that calculates average number of steps for users', function() {
   //   user1.activityRecord = [{date: "2019/09/17", steps: 100}, {date: "2019/09/17", steps: 2000}];
   //   user2.activityRecord = [{date: "2019/09/16", steps: 9820}, {date: "2019/09/17", steps: 234}];
-  //   expect(userRepository.calculateAverageSteps("2019/09/17")).to.equal(778);
+  //   expect(userTestRepository.calculateAverageSteps("2019/09/17")).to.equal(778);
   // })
   // it('should have a method that calculates average number of active minutes for users', function() {
   //   user1.activityRecord = [{date: "2019/09/17", minutesActive: 100}, {date: "2019/09/17", minutesActive: 20}];
   //   user2.activityRecord = [{date: "2019/09/16", minutesActive: 78}, {date: "2019/09/17", minutesActive: 12}];
-  //   expect(userRepository.calculateAverageMinutesActive("2019/09/17")).to.equal(44);
+  //   expect(userTestRepository.calculateAverageMinutesActive("2019/09/17")).to.equal(44);
   // })
   ///////Testers
   it('should have a method that calculates average number of active minutes for users', function() {
     user1.activityRecord = [{date: "2019/09/17", minutesActive: 100}, {date: "2019/09/17", minutesActive: 20}];
     user2.activityRecord = [{date: "2019/09/16", minutesActive: 78}, {date: "2019/09/17", minutesActive: 12}];
-    expect(userRepository.calculateAverage("2019/09/17", "minutesActive")).to.equal(44);
+    expect(userTestRepository.calculateAverage("2019/09/17", "minutesActive")).to.equal(44);
   })
 
   it('should have a method that calculates average number of steps for users', function() {
     user1.activityRecord = [{date: "2019/09/17", steps: 100}, {date: "2019/09/17", steps: 2000}];
     user2.activityRecord = [{date: "2019/09/16", steps: 9820}, {date: "2019/09/17", steps: 234}];
-    expect(userRepository.calculateAverage("2019/09/17", "steps")).to.equal(778);
+    expect(userTestRepository.calculateAverage("2019/09/17", "steps")).to.equal(778);
   })
 
   it('should have a method that calculates average number of stairs for users', function() {
     user1.activityRecord = [{date: "2019/09/17", flightsOfStairs: 10}, {date: "2019/09/17", flightsOfStairs: 15}];
     user2.activityRecord = [{date: "2019/09/16", flightsOfStairs: 8}, {date: "2019/09/17", flightsOfStairs: 4}];
-    expect(userRepository.calculateAverage("2019/09/17", "flightsOfStairs")).to.equal(10);
+    expect(userTestRepository.calculateAverage("2019/09/17", "flightsOfStairs")).to.equal(10);
   })
 });
